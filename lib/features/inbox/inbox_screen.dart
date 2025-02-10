ee47a5c02412df99e3694c0cf29f1af516f946b6
jimin <jiminseo0307@gmail.com>
2025-02-10 20:07:42 +0900

    feat: add activity screen with ListTile and RichText

--- lib/features/inbox/inbox_screen.dart
+++ lib/features/inbox/inbox_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/activity/activity_screen.dart';
 
 class InboxScreen extends StatelessWidget {
   const InboxScreen({super.key});
 
   void _onDmPressed() {}
+
+  void _onActivityTab(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => ActivityScreen(),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +32,7 @@ class InboxScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            onTap: () {},
+            onTap: () => _onActivityTab(context),
             title: Text(
               "Activity",
               style: TextStyle(
