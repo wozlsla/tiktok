bba3cfcf3faa5ec1db635fda35ca6ba3cf8b417e
jimin <jiminseo0307@gmail.com>
2025-02-10 20:28:17 +0900

    feat: add dismissible to notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/activity/activity_screen.dart';
 // import 'package:tiktok/features/authentication/sign_up_screen.dart';
 // import 'package:tiktok/features/onboarding/interests_screen.dart';
 import 'package:tiktok/features/main_navigation/main_navigation_screen.dart';
@@ -36,7 +37,7 @@ class TikTokApp extends StatelessWidget {
           ),
         ),
       ),
-      home: MainNavigationScreen(), // SignUpScreen()
+      home: ActivityScreen(), // SignUpScreen()
     );
   }
 }
