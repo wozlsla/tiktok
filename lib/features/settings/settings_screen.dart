5493f1e67df87b335198e34e66902867653390d2
jimin <jiminseo0307@gmail.com>
2025-02-11 18:21:50 +0900

    feat: add settings screen with (async) date and time pickers and AboutListTile
    showDatePicker, showTimePicker, showDateRangePicker

---
+++ lib/features/settings/settings_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1960),
+                lastDate: DateTime(2030),
+              );
+              print(date);
+
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(2025),
+                lastDate: DateTime(2030),
+                // builder: (context, child) {
+                //   return Theme(
+                //     data: ThemeData(
+                //       appBarTheme: AppBarTheme(
+                //         foregroundColor: Colors.white,
+                //         backgroundColor: Colors.black,
+                //       ),
+                //     ),
+                //     child: child!,
+                //   );
+                // },
+              );
+              print(booking);
+            },
+            title: Text("When is your birthday?"),
+          ),
+          AboutListTile(),
+        ],
+      ),
+    );
+  }
+}
