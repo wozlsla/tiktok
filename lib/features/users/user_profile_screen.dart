19572a2616d4e3ae9174ffa6e36dc2f0c7602cb6
jimin <jiminseo0307@gmail.com>
2025-02-11 16:38:43 +0900

    feat: add PersistentTabBar and apply SafeArea

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/users/widgets/persistent_tab_bar.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -12,209 +13,183 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              title: Text("재키"),
-              actions: [
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.gear,
-                    size: Sizes.size20,
-                  ),
-                )
-              ],
-            ),
-            SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    foregroundColor: Colors.teal,
-                    foregroundImage: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/62599036?v=4"),
-                    child: Text("재키"),
-                  ),
-                  Gaps.v20,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "@wozlsla",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: Sizes.size18,
-                        ),
-                      ),
-                      Gaps.h5,
-                      FaIcon(
-                        FontAwesomeIcons.solidCircleCheck,
-                        size: 13,
-                        color: Colors.blue.shade500,
-                      ),
-                    ],
-                  ),
-                  Gaps.v24,
-                  SizedBox(
-                    height: Sizes.size48,
-                    child: Row(
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: Text("재키"),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.gear,
+                      size: Sizes.size20,
+                    ),
+                  )
+                ],
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      foregroundColor: Colors.teal,
+                      foregroundImage: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/62599036?v=4"),
+                      child: Text("재키"),
+                    ),
+                    Gaps.v20,
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        SocialStats(
-                          count: 97,
-                          label: "Follwing",
+                        Text(
+                          "@wozlsla",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size18,
+                          ),
                         ),
-                        VerticalDivider(
-                          width: Sizes.size32, // 간격
-                          thickness: 0.5,
-                          color: Colors.grey.shade400,
-                          indent: Sizes.size12,
-                          endIndent: Sizes.size12,
-                        ),
-                        SocialStats(
-                          count: 12110,
-                          label: "Follwers",
-                        ),
-                        VerticalDivider(
-                          width: Sizes.size32, // 간격
-                          thickness: 0.5,
-                          color: Colors.grey.shade400,
-                          indent: Sizes.size12,
-                          endIndent: Sizes.size12,
-                        ),
-                        SocialStats(
-                          count: 819430000,
-                          label: "Likes",
+                        Gaps.h5,
+                        FaIcon(
+                          FontAwesomeIcons.solidCircleCheck,
+                          size: 13,
+                          color: Colors.blue.shade500,
                         ),
                       ],
                     ),
-                  ),
-                  Gaps.v14,
-                  FractionallySizedBox(
-                    widthFactor: 0.33,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: Sizes.size12,
+                    Gaps.v24,
+                    SizedBox(
+                      height: Sizes.size48,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SocialStats(
+                            count: 97,
+                            label: "Follwing",
+                          ),
+                          VerticalDivider(
+                            width: Sizes.size32, // 간격
+                            thickness: 0.5,
+                            color: Colors.grey.shade400,
+                            indent: Sizes.size12,
+                            endIndent: Sizes.size12,
+                          ),
+                          SocialStats(
+                            count: 12110,
+                            label: "Follwers",
+                          ),
+                          VerticalDivider(
+                            width: Sizes.size32, // 간격
+                            thickness: 0.5,
+                            color: Colors.grey.shade400,
+                            indent: Sizes.size12,
+                            endIndent: Sizes.size12,
+                          ),
+                          SocialStats(
+                            count: 819430000,
+                            label: "Likes",
+                          ),
+                        ],
                       ),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(
-                            Sizes.size4,
+                    ),
+                    Gaps.v14,
+                    FractionallySizedBox(
+                      widthFactor: 0.33,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: Sizes.size12,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              Sizes.size4,
+                            ),
                           ),
                         ),
+                        child: Text(
+                          "Follow",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Gaps.v14,
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: Sizes.size32,
                       ),
                       child: Text(
-                        "Follow",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
+                        "My pooku dog is fluffy like a big big cloud, and its cuteness is absolutely irresistible. Just one cuddle feels like hugging a warm bundle of joy!",
                         textAlign: TextAlign.center,
                       ),
                     ),
-                  ),
-                  Gaps.v14,
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: Sizes.size32,
-                    ),
-                    child: Text(
-                      "My pooku dog is fluffy like a big big cloud, and its cuteness is absolutely irresistible. Just one cuddle feels like hugging a warm bundle of joy!",
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Gaps.v14,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.link,
-                        size: Sizes.size12,
-                      ),
-                      Gaps.h4,
-                      Text(
-                        "https://github.com/wozlsla",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
+                    Gaps.v14,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.link,
+                          size: Sizes.size12,
                         ),
-                      ),
-                    ],
-                  ),
-                  Gaps.v20,
-                  Container(
-                    // tabbar 테두리
-                    decoration: BoxDecoration(
-                      border: Border.symmetric(
-                        horizontal: BorderSide(
-                          color: Colors.grey.shade200,
-                          width: 0.5,
-                        ),
-                      ),
-                    ),
-                    child: TabBar(
-                      indicatorSize: TabBarIndicatorSize.label,
-                      indicatorColor: Colors.black,
-                      labelPadding: EdgeInsets.symmetric(
-                        vertical: Sizes.size10,
-                      ),
-                      labelColor: Colors.black,
-                      tabs: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: Sizes.size20,
-                          ), // label indicator size
-                          child: Icon(Icons.grid_4x4_rounded),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: Sizes.size20,
+                        Gaps.h4,
+                        Text(
+                          "https://github.com/wozlsla",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
                           ),
-                          child: FaIcon(FontAwesomeIcons.heart),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    Gaps.v20,
+                  ],
+                ),
               ),
-            ),
-          ];
-        },
-        body: TabBarView(
-          children: [
-            GridView.builder(
-              // physics: NeverScrollableScrollPhysics(),
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              itemCount: 20,
-              padding: EdgeInsets.zero,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: Sizes.size2,
-                mainAxisSpacing: Sizes.size2,
-                childAspectRatio: 9 / 14,
+              SliverPersistentHeader(
+                delegate: PersistentTabBar(),
+                pinned: true,
               ),
-              itemBuilder: (context, index) => Column(
-                children: [
-                  AspectRatio(
-                    aspectRatio: 9 / 14,
-                    child: FadeInImage.assetNetwork(
-                      fit: BoxFit.cover,
-                      placeholder: "assets/images/pooku_sleep.jpg",
-                      image:
-                          "https://picsum.photos/seed/${index + 1}/200/${350 + (5 * index)}",
+            ];
+          },
+          body: TabBarView(
+            children: [
+              GridView.builder(
+                // physics: NeverScrollableScrollPhysics(),
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.onDrag,
+                itemCount: 20,
+                padding: EdgeInsets.zero,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: Sizes.size2,
+                  mainAxisSpacing: Sizes.size2,
+                  childAspectRatio: 9 / 14,
+                ),
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 9 / 14,
+                      child: FadeInImage.assetNetwork(
+                        fit: BoxFit.cover,
+                        placeholder: "assets/images/pooku_sleep.jpg",
+                        image:
+                            "https://picsum.photos/seed/${index + 1}/200/${350 + (5 * index)}",
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Center(
-              child: Text("Page 2"),
-            ),
-          ],
+              Center(
+                child: Text("Page 2"),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/users/widgets/persistent_tab_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/sizes.dart';
+
+class PersistentTabBar extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        // tabbar 테두리
+        border: Border.symmetric(
+          horizontal: BorderSide(
+            color: Colors.grey.shade200,
+            width: 0.5,
+          ),
+        ),
+      ),
+      child: TabBar(
+        indicatorSize: TabBarIndicatorSize.label,
+        indicatorColor: Colors.black,
+        labelPadding: EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        labelColor: Colors.black,
+        tabs: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ), // label indicator size
+            child: Icon(Icons.grid_4x4_rounded),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: FaIcon(FontAwesomeIcons.heart),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 47;
+
+  @override
+  double get minExtent => 47;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
