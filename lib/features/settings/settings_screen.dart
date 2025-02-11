f71d40fb04159379def0a3ac51e26ff7947dcd9e
jimin <jiminseo0307@gmail.com>
2025-02-11 18:42:02 +0900

    feat: add SwitchListTile and CheckboxListTile
    CupertinoSwitch, SwitchListTile, SwitchListTile.adaptive, Switch, Switch.adaptive, Checkbox, CheckboxListTile, ..

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue; // bool 변경
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +26,20 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          SwitchListTile.adaptive(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+            subtitle: const Text("They will be cute."),
+          ),
+          CheckboxListTile(
+            activeColor: Theme.of(context).primaryColor,
+            // checkColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Marketing emails"),
+            subtitle: const Text("We won't spam you."),
+          ),
           ListTile(
             onTap: () async {
               final date = await showDatePicker(
@@ -46,7 +74,8 @@ class SettingsScreen extends StatelessWidget {
               );
               print(booking);
             },
-            title: Text("When is your birthday?"),
+            title: const Text("What is your birthday?"),
+            subtitle: const Text("I need to know!"),
           ),
           AboutListTile(),
         ],
