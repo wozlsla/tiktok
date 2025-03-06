ee5d9882b84eca454306e5d5121ba29b32d7fcf7
jimin <jiminseo0307@gmail.com>
2025-03-07 03:42:18 +0900

    feat: extract thumbnails - 1
    - use ffmpeg
    - issues...

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,52 @@
 import * as admin from "firebase-admin";
+import { getStorage } from "firebase-admin/storage";
 import { onDocumentCreated } from "firebase-functions/v2/firestore";
-
+// const { spawn } = require("child-process-promise");
 
 admin.initializeApp();
 
-// export const onVideoCreated = functions.firestore.document("videos/{videoId}").onCreate(async (snapshot, context) => {
-//     await snapshot.ref.update({"hello":"from functions"});
-// });
-
 export const onVideoCreated = onDocumentCreated(
-    {
-        document: "videos/{videoId}", 
-        region: "asia-northeast3",
-    }, 
-    async (event) => {
+  {
+    document: "videos/{videoId}",
+    region: "asia-northeast3",
+  },
+  async (event) => {
     const snapshot = event.data;
     if (!snapshot) return;
 
-    await snapshot.ref.update({"hello":"from functions"});
-});
+    const video = snapshot.data();
+
+    const spawn = require("child-process-promise").spawn;
+
+    const tempPath = `/tmp/${snapshot.id}.jpg`;
+    try {
+      // FFmpeg를 사용하여 썸네일 생성
+      await spawn("ffmpeg", [
+        "-i",
+        video.fileUrl,
+        "-ss",
+        "00:00:01.000",
+        "-vframes",
+        "1",
+        "-vf",
+        "scale=150:-1",
+        tempPath,
+      ]);
+
+      console.log(`Thumbnail created at ${tempPath}`);
+
+      // Firebase Storage에 업로드
+      const storage = getStorage().bucket();
+      await storage.upload(tempPath, {
+        destination: `thumbnails/${snapshot.id}.jpg`,
+        metadata: {
+          contentType: "image/jpg",
+        },
+      });
+    } catch (error) {
+      console.error("Error processing video:", error);
+    }
+  }
+);
+
+// firebase functions:log
